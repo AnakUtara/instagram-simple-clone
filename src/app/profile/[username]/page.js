e711b592f37d6771ecd88cd79953e0b3f8bf1f4a
astnab <astnab@gmail.com>
2024-04-23 13:39:59 +0700

    Update page.js

--- src/app/profile/[username]/page.js
+++ src/app/profile/[username]/page.js
@@ -2,13 +2,12 @@
 import React from "react";
 import Header from "./_components/header";
 import Photos from "./_components/photos";
-import { photosData } from "./_sources/profileData";
 
 export default function Profile () {
 
     return (        
         <>
-        <Header photosData={photosData}/>
+        <Header />
         <Photos />
         </>
     )
