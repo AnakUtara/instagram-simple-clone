315b4a3beda52886e4c62903e8b7aedea01031b8
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2024-04-23 15:12:42 +0700

    install json-server

--- src/app/[username]/layout.jsx
+++ src/app/[username]/layout.jsx
@@ -4,13 +4,11 @@ import MobileHeader from "../_ui/home/MobileHeader";
 
 export default function ProfileLayout({ children }) {
 	return (
-		<>
-			<div className="flex flex-col sm:flex-row">
-				<MobileHeader />
-				<CreateModal />
-				{children}
-				<MainNav />
-			</div>
-		</>
+		<div className="flex flex-col sm:flex-row">
+			<MobileHeader />
+			<CreateModal />
+			{children}
+			<MainNav />
+		</div>
 	);
 }
